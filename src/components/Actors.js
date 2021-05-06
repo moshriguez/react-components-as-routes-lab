e82ee221b38a2e569d82fb605e4b0dd850fa1e93
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-05-06 15:11:46 -0700

    completed

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  const renderActors = () => {
+    return actors.map(actor => {
+      return (
+        <div>
+          <h2>{actor.name}</h2>
+          <ul>
+          {actor.movies.map(movie => <li>{movie}</li>)}
+          </ul>
+        </div>
+      )
+    })
+  }
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const renderDirectors = () => {
+    return directors.map(director => {
+      return (
+        <div>
+          <h2>{director.name}</h2>
+          <ul>
+          {director.movies.map(movie => <li>{movie}</li>)}
+          </ul>
+        </div>
+      )
+    })
+  }
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {renderDirectors()}
     </div>
   );
 }
