676f32ac7553f9925153b3f658b43a5eca5ca0fc
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-05-06 15:11:34 -0700

    commit tree 45dbc0ac12a9c2664517e0b9a26611e7a3c575e9

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  const renderMovies = () => {
+    return movies.map(movie => {
+      return (
+        <div>
+          <h2>{movie.title}</h2>
+          <p>{movie.time} minutes</p>
+          <ul>
+          {movie.genres.map(genre => <li>{genre}</li>)}
+          </ul>
+        </div>
+      )
+    })
+  }
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {renderMovies()}
     </div>
   );
 };
